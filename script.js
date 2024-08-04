de776b85535a17ad118eaf1e572da9846dc8fb27
009661@#m <tari.almutairi1995@gmai.com>
2024-08-04 22:38:36 +0300

    Rename to index

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json
       let title = document.createElement("h3");
       title.textContent = books[i].title;
       title.id = 'p';
+      title.style.fontFamily = "'Times New Roman', Times, serif";
       box.appendChild(title);
 
       let image = document.createElement("img");
@@ -21,6 +22,10 @@ fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json
       let description = document.createElement("h3");
       description.textContent = books[i].description;
       description.id = 'p'; 
+      description.style.fontFamily = "'Times New Roman', Times, serif";
+      description.style.lineHeight = "1.6";
+      description.style.textAlign = "left";
+      description.style.fontSize = "90%"; 
       box.appendChild(description);
 
       containerr.appendChild(box);
@@ -31,9 +36,7 @@ fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json
 
 
 let button = document.getElementById("buten");
-button.addEventListener("click", createAccount);
-
-function createAccount() {
+button.addEventListener("click", () => {
     let username = document.getElementById('username').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
@@ -53,33 +56,31 @@ function createAccount() {
         return;
     }
 
-    fetch('https://6552c0675449cfda0f2dca61.mockapi.io/books', {
-        method: 'POST',
+    fetch("https://669b7915276e45187d3593df.mockapi.io/signt", {
+        method: "POST",
         body: JSON.stringify({
             username: username,
-            email: email,
-            password: password
+            email : email,
+            password: password,
         }),
         headers: {
-            'Content-Type': 'application/json'
-        }
+            "Content-type": "application/json; charset=UTF-8",
+        },
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        alert("تم إنشاء الحساب بنجاح!");
-        window.location.href = "index.html";
-    })
-    .catch(error => {
-        console.error(error);
-        alert("حدث خطأ أثناء إنشاء الحساب. يرجى المحاولة مرة أخرى.");
+    .then((response) => response.json())
+    .then((json) => {
+        window.location.href ="indexx.html";
     });
-}
+});
+
 
 function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
+
+
+
 
 
 
